c3a0c08a29661acd4a8e4bc85f0d083c3b5efe6a
Qianru Zhang <qz2432@nyu.edu>
2024-09-18 18:25:05 -0400

    starter template init

--- Starter Template Optimized/src/main.cpp
+++ Starter Template Optimized/src/main.cpp
@@ -1,10 +1,11 @@
 #include <mbed.h>
 
 int main() {
-
+  int number = 0;
   // put your setup code here, to run once:
 
   while(1) {
     // put your main code here, to run repeatedly:
+    number += 1;
   }
 }
